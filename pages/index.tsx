b1fbeb685cf27764f56794a3239315540d4ab5d3
Azaeres <azaeres@gmail.com>
2023-08-02 22:20:28 -0700

    Fixes for update lifecycle logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,18 +11,15 @@ export default function Home() {
     ) {
       const wb = window.workbox
 
-      const showSkipWaitingPrompt = async () => {
-        // Assuming the user accepted the update, set up a listener
-        // that will reload the page as soon as the previously waiting
-        // service worker has taken control.
-        wb.addEventListener('controlling', () => {
-          // At this point, reloading will ensure that the current
-          // tab is loaded under the control of the new service worker.
-          // Depending on your web app, you may want to auto-save or
-          // persist transient state before triggering the reload.
-          window.location.reload()
-        })
+      // add event listeners to handle PWA lifecycle events
+      wb.addEventListener('installed', (event) => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
 
+      // Add an event listener to detect when the registered
+      // service worker has installed but is waiting to activate.
+      wb.addEventListener('waiting', () => {
         // When `event.wasWaitingBeforeRegister` is true, a previously
         // updated service worker is still waiting.
         // You may want to customize the UI prompt accordingly.
@@ -34,24 +31,23 @@ export default function Home() {
         // https://open-ui.org/components/toast.research
         if (confirm('A newer version of this web app is available, reload to update?')) {
           // Send a message to the waiting service worker, instructing it to activate.
+          // Assuming the user accepted the update, set up a listener
+          // that will reload the page as soon as the previously waiting
+          // service worker has taken control.
+          wb.addEventListener('controlling', () => {
+            // At this point, reloading will ensure that the current
+            // tab is loaded under the control of the new service worker.
+            // Depending on your web app, you may want to auto-save or
+            // persist transient state before triggering the reload.
+            window.location.reload()
+          })
+
           wb.messageSkipWaiting()
         } else {
           console.log(
             'User rejected to update SW, keeping the old version. New version will be automatically loaded when the app is opened next time.'
           )
         }
-      }
-
-      // add event listeners to handle PWA lifecycle events
-      wb.addEventListener('installed', (event) => {
-        console.log(`Event ${event.type} is triggered.`)
-        console.log(event)
-      })
-
-      // Add an event listener to detect when the registered
-      // service worker has installed but is waiting to activate.
-      wb.addEventListener('waiting', () => {
-        showSkipWaitingPrompt()
       })
 
       wb.addEventListener('controlling', (event) => {
