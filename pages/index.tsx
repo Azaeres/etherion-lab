e45c271c5e0589c7a9a36776211c1aa66cfc366f
Azaeres <azaeres@gmail.com>
2023-08-02 22:28:42 -0700

    Log decoration

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ export default function Home() {
       'serviceWorker' in navigator &&
       window.workbox !== undefined
     ) {
+      console.log('Home useEffect  :')
       const wb = window.workbox
       let reloading = false
 
@@ -21,6 +22,7 @@ export default function Home() {
       // Add an event listener to detect when the registered
       // service worker has installed but is waiting to activate.
       wb.addEventListener('waiting', () => {
+        console.log('waiting event triggered  :')
         // When `event.wasWaitingBeforeRegister` is true, a previously
         // updated service worker is still waiting.
         // You may want to customize the UI prompt accordingly.
@@ -31,21 +33,25 @@ export default function Home() {
         // https://open-ui.org/components/alert.research or
         // https://open-ui.org/components/toast.research
         if (confirm('A newer version of this web app is available, reload to update?')) {
+          console.log('user confirmed reload')
           // Send a message to the waiting service worker, instructing it to activate.
           // Assuming the user accepted the update, set up a listener
           // that will reload the page as soon as the previously waiting
           // service worker has taken control.
           wb.addEventListener('controlling', () => {
+            console.log('user confirmed reload - controlling event triggered  :')
             // At this point, reloading will ensure that the current
             // tab is loaded under the control of the new service worker.
             // Depending on your web app, you may want to auto-save or
             // persist transient state before triggering the reload.
             if (!reloading) {
+              console.log('reloading...  :')
               reloading = true
               window.location.reload()
             }
           })
 
+          console.log('triggering SKIP_WAITING  :')
           wb.messageSkipWaiting()
         } else {
           console.log(
@@ -64,6 +70,7 @@ export default function Home() {
         console.log(event)
       })
 
+      console.log('wb.register()  :')
       // never forget to call register as automatic registration is turned off in next.config.js
       wb.register()
     }
