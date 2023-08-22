d7588a09abc85546180492fbd176377e1bbc5fe2
Azaeres <azaeres@gmail.com>
2023-08-22 14:38:56 -0700

    Using next-pwa skipWaiting plugin

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
+  skipWaiting: true,
 })
 
 /** @type {import("next").NextConfig} */
