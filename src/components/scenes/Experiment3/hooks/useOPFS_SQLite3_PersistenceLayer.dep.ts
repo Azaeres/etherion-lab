f1cef668e38e913f1956dd7b5ff9e6d0f120c5e1
Azaeres <azaeres@gmail.com>
2024-08-08 16:30:37 -0700

    Upgrade next-pwa, cleanup

---
+++ src/components/scenes/Experiment3/hooks/useOPFS_SQLite3_PersistenceLayer.dep.ts
@@ -0,0 +1,25 @@
+// import { useCallback, useEffect, useMemo } from 'react'
+
+export default function useOPFS_SQLite3_PersistenceLayer() {
+  // const onWorkerMessage = useCallback((event: MessageEvent<unknown>) => {
+  //   console.log('Host received:', event.data)
+  // }, [])
+  // const worker = useMemo(() => {
+  //   console.log('Creating new worker...  :')
+  //   // https://webpack.js.org/guides/web-workers/
+  //   return new Worker(new URL('src/example.worker.js', import.meta.url))
+  // }, [])
+  // useEffect(() => {
+  //   console.log('Adding event listener...  :')
+  //   worker.onmessage = onWorkerMessage
+  //   // worker.addEventListener('message', onWorkerMessage)
+  //   setTimeout(() => {
+  //     console.log('Posting message to web worker...  :', worker)
+  //     worker.postMessage('from Host')
+  //   }, 2000)
+  //   return () => {
+  //     worker.terminate()
+  //   }
+  // }, [onWorkerMessage, worker])
+  // return worker
+}
