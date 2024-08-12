39df2c485684cc7bff059a65581b3537dc7e5406
Azaeres <azaeres@gmail.com>
2024-08-12 02:36:03 -0700

    Fix for camera shake issue on parallax demo

--- src/components/scenes/Experiment1/index.tsx
+++ src/components/scenes/Experiment1/index.tsx
@@ -7,17 +7,20 @@ import { Spring } from 'react-spring'
 import { Sprite } from '@pixi/react-animated'
 import logo from 'src/components/Logo/etherion-logo.png'
 import { OPTIONS } from 'src/components/PixiStage'
-import { useCallback, useContext, useMemo, useState } from 'react'
+import { useCallback, useContext, useEffect, useMemo, useState } from 'react'
 import {
   ParallaxCameraContext,
   ParallaxCameraProvider,
   ParallaxLayer,
   useParallaxCameraRef,
 } from 'pixi-react-parallax'
+import type { ParallaxCamera } from 'pixi-react-parallax'
 import { Text } from '@pixi/react'
 import Button from '../Experiment2/Button'
 import Overlay from '../Experiment2/Overlay'
 import DebugIndicator from 'src/components/DebugIndicator'
+
+const texture = Texture.from(stars.src)
 
 const set = (): AnimatedLogoProps => ({
   x: Math.random() * OPTIONS.width,
@@ -36,7 +39,12 @@ const getRandomPosition = () => {
 export default function Experiment1() {
   const navigate = useNextjsNavigate()
   const [transform, setTransform] = useState<AnimatedLogoProps>(set)
-  const click = useCallback(() => {
+  const [camera, setCamera] = useState<ParallaxCamera | null>()
+  const onCamera = useCallback(setCamera, [setCamera])
+  const pointerDown = useCallback(() => {
+    camera?.shake(40, 0.2)
+  }, [camera])
+  const pointerUp = useCallback(() => {
     setTransform(set)
   }, [])
   const logo1 = useMemo(getRandomPosition, [])
@@ -47,7 +55,7 @@ export default function Experiment1() {
     <>
       <ParallaxCameraProvider>
         <ParallaxLayer zIndex={-18000}>
-          <Sprite texture={Texture.from(stars.src)} x={0} y={0} anchor={0.5} scale={65} />
+          <Sprite texture={texture} x={0} y={0} anchor={0.5} scale={65} />
         </ParallaxLayer>
         <ParallaxLayer zIndex={-850}>
           {/* Very far away from the camera */}
@@ -63,7 +71,7 @@ export default function Experiment1() {
         </ParallaxLayer>
         <ParallaxLayer zIndex={0}>
           {/* Normal distance. No auto-scaling applied. */}
-          <AnimatedLogo {...transform} />
+          <AnimatedLogo {...transform} onCamera={onCamera} />
         </ParallaxLayer>
         <ParallaxLayer zIndex={100}>
           {/* Don't exceed the focal length (default 300) or it'll pass behind the camera. Technically, it gets mirrored, and looks weird. */}
@@ -80,13 +88,13 @@ export default function Experiment1() {
         style={new TextStyle({ fill: '0xffffff', fontSize: '38px' })}
       />
       <Text
-        text={`Click/tap anywhere to move the camera target.\nClick/tap on the camera target to also cause the camera to shake!`}
+        text={`Click/tap down anywhere to shake the camera.\nClick/tap up to move the camera target to a random location!`}
         x={100}
         y={200}
         scale={2}
         style={new TextStyle({ fill: '0xcccccc', fontSize: '22px' })}
       />
-      <Overlay onPress={click} />
+      <Overlay onPointerDown={pointerDown} onPointerUp={pointerUp} />
       <Button text="&equiv; Menu" x={100} y={50} width={300} height={100} onPress={navigate('/')} />
     </>
   )
@@ -96,30 +104,26 @@ interface AnimatedLogoProps {
   y: number
   rotation: number
   // scale: number
+  onCamera?: (camera: ParallaxCamera) => void
 }
 
 function AnimatedLogo(props: AnimatedLogoProps) {
+  const springProps = { ...props }
+  delete springProps.onCamera
   const [, setCameraTargetRef] = useParallaxCameraRef()
   const camera = useContext(ParallaxCameraContext)
-  const click = useCallback(() => {
-    camera?.shake(40, 0.2)
+  const { onCamera } = props
+  useEffect(() => {
+    camera && onCamera?.(camera)
   }, [camera])
   return (
     <Spring
-      to={props}
+      to={springProps}
       config={{ mass: 10, tension: 1000, friction: 100 /*, duration: 4000 */ }}
       loop
     >
       {(props) => (
-        <Sprite
-          anchor={0.5}
-          texture={Texture.from(logo.src)}
-          {...props}
-          onpointerup={click}
-          cursor="pointer"
-          eventMode="dynamic"
-          ref={setCameraTargetRef}
-        />
+        <Sprite anchor={0.5} texture={Texture.from(logo.src)} {...props} ref={setCameraTargetRef} />
       )}
     </Spring>
   )
